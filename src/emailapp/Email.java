8c294876b9f71e8351697ba7696bf4f12c14207d
wyu6609 <wy427@nyu.edu>
2022-09-19 17:25:12 -0400

    created email constructor and setDepartment method

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -1,5 +1,7 @@
 package emailapp;
 
+import java.util.Scanner;
+
 public class Email {
     private String firstName;
     private String lastName;
@@ -14,9 +16,26 @@ public class Email {
         this.lastName = lastName;
         System.out.println("EMAIL CREATED: " + this.firstName + "" + this.lastName);
 
+        //Call a method asking for the department - return the department
+        this.department = setDepartment();
+        System.out.println("Department: " + this.department);
     }
     //Ask for the department
+    private String setDepartment() {
+       System.out.print("DEPARTMENT CODES\n1 for Sales\n2 for Development\n3 for Accounting\n0 for none\nEnter the department code: ");
+       Scanner in = new Scanner(System.in);
+       int depChoice = in.nextInt();
 
+       if (depChoice == 1){
+           return "Sales";
+       } else if (depChoice == 2){
+           return "dev";
+       } else if (depChoice == 3){
+           return "acct";
+       } else {return "";}
+
+
+    }
     //Generate a random password
 
     // set the mailbox capacity
