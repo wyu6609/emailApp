7c22054a632449f608affe954701de51e8607217
wyu6609 <wy427@nyu.edu>
2022-09-19 17:15:16 -0400

    first commit

---
+++ src/emailapp/Email.java
@@ -0,0 +1,28 @@
+package emailapp;
+
+public class Email {
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String department;
+    private int mailBoxCapacity;
+    private String alternateEmail;
+
+    //Constructor to receive the first name and last name
+    public Email(String firstName, String lastName){
+        this.firstName  = firstName;
+        this.lastName = lastName;
+        System.out.println("EMAIL CREATED: " + this.firstName + "" + this.lastName);
+
+    }
+    //Ask for the department
+
+    //Generate a random password
+
+    // set the mailbox capacity
+
+    //Set the alternate email
+
+    // change the password
+
+}
