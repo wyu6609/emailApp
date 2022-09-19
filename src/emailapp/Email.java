ca2f605686ab6a65a25724e9929957b646427658
wyu6609 <wy427@nyu.edu>
2022-09-19 17:30:44 -0400

    created randomPassword method

--- src/emailapp/Email.java
+++ src/emailapp/Email.java
@@ -19,6 +19,8 @@ public class Email {
         //Call a method asking for the department - return the department
         this.department = setDepartment();
         System.out.println("Department: " + this.department);
+
+        //Call a method that returns a random password
     }
     //Ask for the department
     private String setDepartment() {
@@ -36,7 +38,18 @@ public class Email {
 
 
     }
+
     //Generate a random password
+    private String randomPassword(int length){
+        String passwordSet = "ABCDEFGHIJKLMNOPQRSTUWXYZ0123456789!@#$%";
+        char[] password = new char[length];
+        for (int i =0; i<length; i++){
+           int rand = (int) Math.random() * passwordSet.length();
+           password[i] = passwordSet.charAt(rand);
+        }
+        return new String(password);
+    }
+
 
     // set the mailbox capacity
 
